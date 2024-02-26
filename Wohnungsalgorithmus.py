6012b973c5ee1e93c67c229aed217ab8469007e0
MicFisch14 <156675747+MicFisch14@users.noreply.github.com>
2024-02-26 13:39:51 +0100

    Small refactoring to Wohnungsalgorithmus

--- Wohnungsalgorithmus.py
+++ Wohnungsalgorithmus.py
@@ -11,11 +11,11 @@ while raumZaehler < raumAnzahl:
     print("---------------")
     raumName = input("Wie ist der Raumname? ")
     raumNamenListe.append(raumName)
-    anzahlQuadrate = int(input("Zimmer " + str(raumZaehler+1) +  ": Wie viele Quadrate sind im Raum? "))
+    anzahlQuadrate = int(input(str(raumNamenListe[raumZaehler]) +  ": In Wieviele Quadtrate soll der Raum geteilt werden? "))
     raumGroeße = 0
     while quadratImRaumZaehler < anzahlQuadrate:
         print("---------------")
-        print("Zimmer " + str(raumZaehler+1) + " - Quadrat " + str(quadratImRaumZaehler+1))
+        print(str(raumNamenListe[raumZaehler]) + " - Quadrat " + str(quadratImRaumZaehler+1))
         laenge = int(input("Geben sie die Raumlänge ein "))
         breite = int(input("Geben sie die Breite ein "))
         raumGroeße += laenge * breite
@@ -27,6 +27,7 @@ while raumZaehler < raumAnzahl:
 durchschnittsRaumgroeße = wohnungsGroeße / raumAnzahl
 print("\n---------------\n")
 raumPrintZaehler = 0
+print("\nAbfrage abgeschlossen\n")
 while raumPrintZaehler < raumAnzahl:
     print(str(raumNamenListe[raumPrintZaehler]) + ": " + str(raumGroeßenListe[raumPrintZaehler]) + "qm")
     raumPrintZaehler += 1
